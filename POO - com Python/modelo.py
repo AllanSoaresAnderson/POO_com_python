80ae0b558babb51f008323f1db85272e0ad92f3c
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-06-06 22:18:23 -0300

    Maleficios de extender uma classe complexa

--- POO - com Python/modelo.py
+++ POO - com Python/modelo.py
@@ -39,10 +39,17 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self.nome}, Ano: {self.ano}, {self.temporada} temporadas, Likes: {self.likes}'
 
-class Playlist (list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 
 vingadores = Filme("vingadores ultimato", 2018, 60)
@@ -70,5 +77,5 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep, gotham]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in playlist_fim_de_semana:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
