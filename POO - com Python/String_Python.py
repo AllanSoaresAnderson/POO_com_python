a7db8ac5135490a1451bbf512d39c50e475dc7f3
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-06-08 16:29:40 -0300

    Regular Expressions - padrões ()?

--- POO - com Python/String_Python.py
+++ POO - com Python/String_Python.py
@@ -1,12 +1,49 @@
-endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440120"
+import re
 
 
-import re  # Regular Expression -- RegEx
+class ExtratorURL:
+    def __init__(self, url):
+        self.url = self.sanitiza_url(url)
+        self.valida_url()
 
-# 5 dígitos + hífen (opcional) + 3 dígitos
+    def sanitiza_url(self, url):
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ""
 
-padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
-busca = padrao.search(endereco)  # Match
-if busca:
-    cep = busca.group()
-    print(cep)
+    def valida_url(self):
+        if not self.url:
+            raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
+
+    def get_url_base(self):
+        indice_interrogacao = self.url.find('?')
+        url_base = self.url[:indice_interrogacao]
+        return url_base
+
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find('?')
+        url_parametros = self.url[indice_interrogacao + 1:]
+        return url_parametros
+
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
+        return valor
+
+
+url = "bytebank.com/xyz?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+print(valor_quantidade)
+
