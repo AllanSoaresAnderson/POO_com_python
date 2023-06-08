4c7cd064275cdf00a37f4492f3ac7a0917a8a362
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-06-08 14:51:00 -0300

    método len

--- POO - com Python/String_Python.py
+++ POO - com Python/String_Python.py
@@ -1,9 +1,17 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?moedaDestino=dolarmoedaOrigem=real"
 
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
-print(url_base)
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+
+print(indice_parametro)
+
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print(valor)
